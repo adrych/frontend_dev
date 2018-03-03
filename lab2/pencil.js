5300c7b1139dd522cbf7e12f54fcbb6648e667b6
adrych <dadrychowski@gmail.com>
2018-03-03 11:17:17 +0100

    new class

--- lab2/pencil.js
+++ lab2/pencil.js
@@ -3,6 +3,20 @@ var pencil = {};
 
 pencil = (function() {
     var MyInterface = {};
+    var softness = ['9H','8H','7H','6H','5H','HB','HH'];
+    var colour =['black','blue','red'];
+    var brand;
+
+    var maintanance_add = function(where, what){
+        return where(what);
+    };
+
+    function add()
+
     
+
     return MyInterface;    
 })();
+
+
+console.log(pencil.brand);
