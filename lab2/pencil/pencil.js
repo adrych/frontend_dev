62d40f4680afb19cf369c3d2537e668b83a6a006
adrych <dadrychowski@gmail.com>
2018-03-13 22:07:54 +0100

    experiments

--- lab2/pencil/pencil.js
+++ lab2/pencil/pencil.js
@@ -2,21 +2,27 @@ var pencil = {};
 
 
 pencil = (function() {
-    var MyInterface = {};
+    var MyInterface = {}
     var softness = ['9H','8H','7H','6H','5H','HB','HH'];
     var colour =['black','blue','red'];
     var brand;
+    
 
-    var maintanance_add = function(where, what){
+    var maintanance = function(where, what){
         return where(what);
     };
 
-    function add()
+    var softnes_adding = function(a){
+        softness.push(a)
+        return a
+    }
 
-    
+    MyInterface.softness = softness;
+    MyInterface.colour = colour;
 
     return MyInterface;    
 })();
 
 
-console.log(pencil.brand);
+console.log(pencil.softness);
+console.log(pencil.colour[1]);
