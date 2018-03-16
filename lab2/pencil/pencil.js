f279568167855f1e072a60aa0fee61e03ebce054
adrych <dadrychowski@gmail.com>
2018-03-16 22:22:43 +0100

    working copy

--- lab2/pencil/pencil.js
+++ lab2/pencil/pencil.js
@@ -2,27 +2,38 @@ var pencil = {};
 
 
 pencil = (function() {
-    var MyInterface = {}
+//var pencil = function(parameter) {
+    //var MyInterface = {}
     var softness = ['9H','8H','7H','6H','5H','HB','HH'];
     var colour =['black','blue','red'];
     var brand;
-    
 
-    var maintanance = function(where, what){
-        return where(what);
-    };
+    //MyInterface.softness = softness;
+    //MyInterface.colour = colour;
+    return {
+        add_softness: function(soft){
+            softness.push(soft);
+            return 'Added: '+soft+ ' '+softness;
+        },
 
-    var softnes_adding = function(a){
-        softness.push(a)
-        return a
+        get_softness: function (s_id) {
+            return softness[s_id];
+        },
+        get_colour: function (s_id) {
+            return colour[s_id];
+        },
+        get_color: function (s_id) {
+            return colour[s_id];
+        }
     }
-
-    MyInterface.softness = softness;
-    MyInterface.colour = colour;
-
-    return MyInterface;    
 })();
 
 
-console.log(pencil.softness);
-console.log(pencil.colour[1]);
+
+console.log(pencil.get_softness(2));
+console.log(pencil.add_softness('D'));
+console.log(pencil.get_color(2));
+
+//console.log(pencil.get_softness(1));
+//console.log(pencil.softness);
+//console.log(pencil.colour[1]);
